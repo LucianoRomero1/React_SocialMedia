0795f48a884f3cbc17b38987ca6a582e71ed6de7
LucianoRomero1 <lucianooromero1@gmail.com>
2023-01-13 12:11:35 -0300

    Basic structure for the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import "./assets/fonts/fontawesome-free-6.1.2-web/css/all.css";
+import "./assets/css/normalize.css";
+import "./assets/css/styles.css";
+import "./assets/css/responsive.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
