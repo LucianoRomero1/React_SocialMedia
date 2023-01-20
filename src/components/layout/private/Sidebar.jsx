e67aefc688f110b7a3672723e23bfb19470b5ed3
LucianoRomero1 <lucianooromero1@gmail.com>
2023-01-20 11:46:58 -0300

    save post

--- src/components/layout/private/Sidebar.jsx
+++ src/components/layout/private/Sidebar.jsx
@@ -1,11 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import avatar from "../../../assets/img/user.png";
 import { Global } from "../../../helpers/Global";
 import useAuth from "../../../hooks/useAuth";
+import { useForm } from "../../../hooks/useForm";
 
 export const Sidebar = () => {
   const { auth, counters } = useAuth();
+  const { form, changed } = useForm({});
+  const [stored, setStored] = useState("not_stored");
+  const token = localStorage.getItem("token");
+
+  const savePublication = async (e) => {
+    e.preventDefault();
+
+    let newPublication = form;
+    newPublication.user = auth._id;
+
+    const request = await fetch(Global.url + "publication/save", {
+      method: "POST",
+      body: JSON.stringify(newPublication),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    });
+
+    const data = await request.json();
+    if (data.status == "success") {
+      setStored("stored");
+    } else {
+      setStored("error");
+    }
+
+    const fileInput = document.querySelector("#file");
+    if (data.status == "success" && fileInput.files[0]) {
+      const formData = new FormData();
+      formData.append("file0", fileInput.files[0]);
+
+      const uploadRequest = await fetch(
+        Global.url + "publication/upload/" + data.publicationStored._id,
+        {
+          method: "POST",
+          body: formData,
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+
+      const uploadData = await uploadRequest.json();
+      if (uploadData.status == "success") {
+        setStored("stored");
+      } else {
+        setStored("error");
+      }
+
+      if (data.status == "success" && uploadData.status == "success") {
+        const myForm = document.querySelector("#publication-form");
+        myForm.reset();
+      }
+    }
+  };
 
   return (
     <aside className="layout__aside">
@@ -43,7 +99,7 @@ export const Sidebar = () => {
 
           <div className="profile-info__stats">
             <div className="stats__following">
-              <Link to={"following/" + auth._id}  className="following__link">
+              <Link to={"following/" + auth._id} className="following__link">
                 <span className="following__title">Following</span>
                 <span className="following__number">{counters.following}</span>
               </Link>
@@ -67,26 +123,49 @@ export const Sidebar = () => {
         </div>
 
         <div className="aside__container-form">
-          <form className="container-form__form-post">
+          {stored == "stored" ? (
+            <strong className="alert alert-success">Posted successfully</strong>
+          ) : (
+            ""
+          )}
+
+          {stored == "error" ? (
+            <strong className="alert alert-danger">Error uploading post</strong>
+          ) : (
+            ""
+          )}
+          <form
+            id="publication-form"
+            className="container-form__form-post"
+            onSubmit={savePublication}
+          >
             <div className="form-post__inputs">
-              <label htmlFor="post" className="form-post__label">
-                ¿Que estas pesando hoy?
+              <label htmlFor="text" className="form-post__label">
+                What are you thinking today?
               </label>
-              <textarea name="post" className="form-post__textarea"></textarea>
+              <textarea
+                name="text"
+                className="form-post__textarea"
+                onChange={changed}
+              />
             </div>
 
             <div className="form-post__inputs">
-              <label htmlFor="image" className="form-post__label">
-                Sube tu foto
+              <label htmlFor="file0" className="form-post__label">
+                Upload your photo
               </label>
-              <input type="file" name="image" className="form-post__image" />
+              <input
+                type="file"
+                name="file0"
+                id="file"
+                className="form-post__image"
+              />
             </div>
 
             <input
               type="submit"
               value="Enviar"
               className="form-post__btn-submit"
-              disabled
             />
           </form>
         </div>
