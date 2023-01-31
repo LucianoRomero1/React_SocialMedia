33289f8a9b375303a8efae4bd3607f6e3ad6e46b
LucianoRomero1 <lucianooromero1@gmail.com>
2023-01-31 08:16:31 -0300

    Profile finish

--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -5,6 +5,7 @@ import avatar from "../../assets/img/user.png";
 import { GetProfile } from "../../helpers/getProfile";
 import { Global } from "../../helpers/Global";
 import useAuth from "../../hooks/useAuth";
+import { PublicationList } from "../publication/PublicationList";
 
 export const Profile = () => {
   const { auth } = useAuth();
@@ -126,31 +127,6 @@ export const Profile = () => {
     }
   };
 
-  const nextPage = () => {
-    let next = page + 1;
-    setPage(next);
-    getPublications(next);
-  };
-
-  const deletePublication = async (publicationId) => {
-    const request = await fetch(
-      Global.url + "publication/remove/" + publicationId,
-      {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: token,
-        },
-      }
-    );
-
-    const data = await request.json();
-
-    setPage(1);
-    setMore(true);
-    getPublications(1, true);
-  };
-
   return (
     <>
       <br />
@@ -238,77 +214,14 @@ export const Profile = () => {
         </div>
       </header>
 
-      <div className="content__posts">
-        {publications.map((publication) => {
-          return (
-            <article className="posts__post" key={publication._id}>
-              <div className="post__container">
-                <div className="post__image-user">
-                  <Link
-                    to={"/social/profile/" + publication.user._id}
-                    className="post__image-link"
-                  >
-                    {publication.user.image != "default.png" && (
-                      <img
-                        src={
-                          Global.url + "user/avatar/" + publication.user.image
-                        }
-                        className="post__user-image"
-                        alt="Foto de perfil"
-                      />
-                    )}
-                    {publication.user.image == "default.png" && (
-                      <img
-                        src={avatar}
-                        className="post__user-image"
-                        alt="Foto de perfil"
-                      />
-                    )}
-                  </Link>
-                </div>
-
-                <div className="post__body">
-                  <div className="post__user-info">
-                    <a href="#" className="user-info__name">
-                      {publication.user.name + " " + publication.user.surname}
-                    </a>
-                    <span className="user-info__divider"> | </span>
-                    <a href="#" className="user-info__create-date">
-                      {publication.created_at}
-                    </a>
-                  </div>
-
-                  <h4 className="post__content">{publication.text}</h4>
-                  {publication.file && (
-                    <img
-                      src={Global.url + "publication/media/" + publication.file}
-                    />
-                  )}
-                </div>
-              </div>
-
-              {auth._id == publication.user._id && (
-                <div className="post__buttons">
-                  <button
-                    className="post__button"
-                    onClick={() => deletePublication(publication._id)}
-                  >
-                    <i className="fa-solid fa-trash-can"></i>
-                  </button>
-                </div>
-              )}
-            </article>
-          );
-        })}
-      </div>
-
-      {more && (
-        <div className="content__container-btn">
-          <button className="content__btn-more-post" onClick={nextPage}>
-            Show more posts
-          </button>
-        </div>
-      )}
+      <PublicationList
+        publications={publications}
+        getPublications={getPublications}
+        page={page}
+        setPage={setPage}
+        more={more}
+        setMore={setMore}
+      />
       <br />
     </>
   );
